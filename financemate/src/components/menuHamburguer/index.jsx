d79b69ca780cf3a164287150f68600f0c01e480e
Pedro Lucas <pedrolucasmota2005@gmail.com>
2025-04-24 12:58:07 -0300

    V6

--- financemate/src/components/menuHamburguer/index.jsx
+++ financemate/src/components/menuHamburguer/index.jsx
@@ -1,36 +1,89 @@
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
+import MenuLateral from './MenuLateral';
+
 const BarraLateral = styled.div`
-	background-color: #333;
-	width: 80px;
-	color: #ffffff;
+    background-color: #333;
+    width: 80px;
+    color: #ffffff;
     display: flex;
     justify-content: center;
     align-items: flex-start;
-	position: fixed;
-	height: 100%;
-	border: 0px;
-	margin-top: 70px;
+    position: fixed;
+    height: 100%;
+    border: 0px;
+    margin-top: 70px;
+    z-index: 2;
 `;
 
 const BotaoMenu = styled.button`
-	background-color: transparent;
-	border: 0px;
-	color: white;
-	cursor: pointer;
-	margin-top: 10px;
-	font-size: 25px;
-	&:hover {
-		color: #979797;
-	}
+    background-color: transparent;
+    border: 0px;
+    color: white;
+    cursor: pointer;
+    margin-top: 10px;
+    font-size: 25px;
+    &:hover {
+        color: #979797;
+    }
+`;
+
+const MenuContainer = styled.div`
+    width: 250px;
+    margin-left: 80px;
+    margin-top: 70px;
+    background-color: #ffffff;
+    height: 100%;
+    position: fixed;
+    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1); /* Sombra na borda direita */
+    opacity: ${(props) => (props.$isVisible ? 1 : 0)};
+    transform: ${(props) => (props.$isVisible ? 'translateX(0)' : 'translateX(-100%)')};
+    transition: opacity 0.3s ease, transform 0.3s ease;
+    pointer-events: ${(props) => (props.$isVisible ? 'auto' : 'none')};
+    z-index: 1; 
 `;
 
 const MenuHamburguer = () => {
-	return (
-		<BarraLateral>
-			<BotaoMenu>☰</BotaoMenu>
-		</BarraLateral>
-	);
+    const [isMenuVisible, setMenuVisible] = useState(false);
+    const menuRef = useRef(null);
+    const buttonRef = useRef(null);
+
+    const toggleMenu = (event) => {
+        event.stopPropagation();
+        setMenuVisible((prev) => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target) &&
+                buttonRef.current &&
+                !buttonRef.current.contains(event.target)
+            ) {
+                setMenuVisible(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <>
+            <BarraLateral>
+                <BotaoMenu ref={buttonRef} onClick={toggleMenu}>
+                    ☰
+                </BotaoMenu>
+            </BarraLateral>
+            <MenuContainer ref={menuRef} $isVisible={isMenuVisible}>
+                <MenuLateral onClose={() => setMenuVisible(false)} />
+            </MenuContainer>
+        </>
+    );
 };
 
 export default MenuHamburguer;
---
+++ financemate/src/components/menuHamburguer/menuLateral.jsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    ul {
+        list-style: none;
+        width: 100%;
+        padding: 0;
+        margin: 0;
+    }
+    li {
+        width: 100%;
+        padding: 10px 0;
+        cursor: pointer;
+        &:hover {
+            background-color: #cecece;
+        }
+    }
+`;
+
+const FotoPerfil = styled.div`
+	background-color: white;
+	border-radius: 50%;
+	width: 120px;
+	height: 120px;
+	border: 1px solid red;
+	margin-bottom: 10px;
+`;
+
+const Perfil = styled.div`
+    height: 25vh;
+    background-color: #7c7c7c;
+    width: 100%;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-top: 5px;
+    
+`
+
+const TextoPerfil = styled.div`
+	border: 1px solid red;
+`;
+
+const MenuLateral = ({ onClose }) => {
+    return (
+        <Menu>
+            <Perfil>
+                <FotoPerfil>
+                    <img src="" alt="" />
+                </FotoPerfil>
+                <TextoPerfil>Bem Vindo</TextoPerfil>
+            </Perfil>
+            <ul>
+                <li onClick={onClose}>Perfil</li>
+                <li onClick={onClose}>Dashboard</li>
+                <li onClick={onClose}>Despesas</li>
+                <li onClick={onClose}>Receitas</li>
+                <li onClick={onClose}>Cartões</li>
+            </ul>
+        </Menu>
+    );
+};
+
+export default MenuLateral;
